f4b9701b2ad9400f09388061567ab78a0a902220
Anastasia Dovgal <51283960+AnastasiaDovgal@users.noreply.github.com>
2019-10-09 12:16:27 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import ToDoList from './to-do-list';
+import Form from './form';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>My first react app</h1>
+        <ToDoList />
+        <Form />
+      </header>
+    </div>
+  );
+}
+
+// прописывать экспортируемые компоненты
+export default App;
---
+++ to-do-list.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './to-do-list.css';
+
+function ToDoList() {
+    return (
+        <div className="to-do-list">
+            <header className="to-do-list-header">
+                <h1>To-Do List</h1>
+            </header>
+        </div>
+    );
+}
+
+// прописывать экспортируемые компоненты
+export default ToDoList;
